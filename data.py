af53d17f29c04761aca8fa55bcda1a282c20fcd4
Salman Khan <salmanbabuzai@gmail.com>
2023-02-08 09:01:54 -0800

    initial commit

---
+++ data.py
@@ -0,0 +1,17 @@
+#All env URLs
+stg_url = "https://levelshoes-m2-cloud-mage24-stg.vaimo.net"
+prod_url = "https://levelshoes.com"
+
+#URL to be used
+url = stg_url
+
+#Search data
+search_item = "crep"
+
+#Checkout Shipping details
+email = "salman.khan@werplay.com"
+first_name = "salman"
+last_name = "khan"
+phone = "56455666"
+city = "Dubai"
+address1 = "Test Order"
